829876477d39ca8666f617876a8b39d065ec4942
sally2596@gmail.com <sally2596@gmail.com>
2022-02-09 17:11:45 +0900

    Solved Baekjoon_17298

---
+++ Java/Algorithm/src/net/acmicpc/baekjoon_17298.java
@@ -0,0 +1,41 @@
+package net.acmicpc;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class baekjoon_17298 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		int n = Integer.parseInt(bf.readLine());
+		String[] arr = bf.readLine().split(" ");
+
+		Stack<Integer> copy = new Stack<Integer>();
+		int[] ans = new int[n];
+		int index = n - 1;
+
+		ans[index--] = -1;
+		copy.push(Integer.parseInt(arr[n - 1]));
+
+		for (int i = n - 2; i >= 0; i--) {
+			int now = Integer.parseInt(arr[i]);
+			while (!copy.empty() && copy.peek() < now) {
+				copy.pop();
+			}
+			if (copy.empty())
+				ans[index--] = -1;
+			else
+				ans[index--] = copy.peek();
+			copy.push(now);
+		}
+		for (int i = 0; i < n; i++) {
+			sb.append(ans[i] + " ");
+		}
+		System.out.println(sb.toString());
+	}
+
+}
