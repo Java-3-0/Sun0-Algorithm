d4ae002c86021edf5d3d86edfda4cedbfd3b9b96
sally2596 <sally2596@gmail.com>
2022-04-23 03:24:22 +0900

    Solved Baekjoon_12904

--- Java/Algorithm/src/net/acmicpc/baekjoon_12904.java
+++ Java/Algorithm/src/net/acmicpc/baekjoon_12904.java
@@ -1,4 +1,4 @@
-/***
+/*** StringBuilder version
  * 11880kb
  * 92ms
  */
@@ -10,29 +10,71 @@ import java.io.InputStreamReader;
 
 public class baekjoon_12904 {
 
+    //    private static String origin;
+//    private static StringBuilder sb;
+//
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+//        sb = new StringBuilder();
+//
+//        origin = bf.readLine();
+//        sb = new StringBuilder(bf.readLine());
+//
+//        while (origin.length() < sb.length()) {
+//            if (sb.charAt(sb.length() - 1) == 'A')
+//                sb.setLength(sb.length() - 1);
+//            else {
+//                sb.setLength(sb.length() - 1);
+//                sb.reverse();
+//            }
+//        }
+//        if (sb.toString().equals(origin))
+//            System.out.println("1");
+//        else
+//            System.out.println("0");
+//    }
+
+    /*** two-pointer version
+     * 11488kb
+     * 76ms
+     */
     private static String origin;
-    private static StringBuilder sb;
-    private static int a, b;
 
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        sb = new StringBuilder();
+        StringBuilder sb = new StringBuilder();
 
+        boolean isReverse = false;
         origin = bf.readLine();
-        sb = new StringBuilder(bf.readLine());
+        char[] ch = bf.readLine().toCharArray();
+        int l = 0;
+        int r = ch.length - 1;
 
-        while (origin.length() < sb.length()) {
-            if (sb.charAt(sb.length() - 1) == 'A')
-                sb.setLength(sb.length() - 1);
-            else {
-                sb.setLength(sb.length() - 1);
-                sb.reverse();
+        while (origin.length() < (r - l + 1)) {
+            if (isReverse) {
+                if (ch[l] == 'B')
+                    isReverse = false;
+                l++;
+            } else {
+                if (ch[r] == 'B')
+                    isReverse = true;
+                r--;
             }
         }
+
+        if (isReverse) {
+            for (int i = r; i >= l; i--) {
+                sb.append(ch[i]);
+            }
+        } else {
+            for (int i = l; i <= r; i++) {
+                sb.append(ch[i]);
+            }
+        }
+
         if (sb.toString().equals(origin))
             System.out.println("1");
         else
             System.out.println("0");
     }
-
 }
