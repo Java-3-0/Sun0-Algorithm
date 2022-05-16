01b5a629dc1de308b2de46d5ffa9044e042b7345
sally2596 <sally2596@gmail.com>
2022-05-16 22:31:23 +0900

    Solved Baekjoon_1956

--- Java/Algorithm/src/net/acmicpc/baekjoon_1956.java
+++ Java/Algorithm/src/net/acmicpc/baekjoon_1956.java
@@ -1,3 +1,7 @@
+/***
+ * 69480kb
+ * 960ms
+ */
 package net.acmicpc;
 
 import java.io.BufferedReader;
@@ -12,12 +16,10 @@ public class baekjoon_1956 {
 
         int v = Integer.parseInt(st.nextToken());
         int e = Integer.parseInt(st.nextToken());
-        int[][] distance = new int[v][v];
         int[][] arr = new int[v][v];
         for (int i = 0; i < v; i++) {
             for (int j = 0; j < v; j++) {
-                distance[i][j] = 5000000;
-                arr[i][j] = 5000000;
+                arr[i][j] = 500000000;
             }
         }
 
@@ -28,15 +30,14 @@ public class baekjoon_1956 {
             int to = Integer.parseInt(st.nextToken());
             int weight = Integer.parseInt(st.nextToken());
             arr[from - 1][to - 1] = weight;
-            distance[from - 1][to - 1] = weight;
         }
 
         for (int k = 0; k < v; k++) {
             for (int i = 0; i < v; i++) {
+                if (i == k) continue;
                 for (int j = 0; j < v; j++) {
-                    if (arr[i][k] == -1 || arr[k][j] == -1) continue;
-                    if (arr[i][k] + arr[k][j] < distance[i][j])
-                        distance[i][j] = arr[i][k] + arr[k][j];
+                    if (arr[i][k] + arr[k][j] < arr[i][j])
+                        arr[i][j] = arr[i][k] + arr[k][j];
                 }
             }
         }
@@ -45,12 +46,11 @@ public class baekjoon_1956 {
         int num = Integer.MAX_VALUE;
         for (int i = 0; i < v; i++) {
             for (int j = 0; j < v; j++) {
-                if (distance[i][j] == -1 || distance[j][i] == -1)
-                    continue;
-                num = Math.min(num, distance[i][j] + distance[j][i]);
+                if (i == j) continue;
+                num = Math.min(num, arr[i][j] + arr[j][i]);
             }
         }
 
-        System.out.println(num == Integer.MAX_VALUE ? -1 : num);
+        System.out.println(num > 500000000 ? -1 : num);
     }
 }
